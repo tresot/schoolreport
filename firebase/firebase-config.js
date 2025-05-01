bd38f2778c745fe15409f08258e29a0363954d1b
tresot <kingstorm12minecraft@gmail.com>
2025-05-01 18:03:51 +0200

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,21 +1,21 @@
 // firebase/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
 
 // Configuration Firebase de ton projet SchoolReport
 const firebaseConfig = {
     apiKey: "AIzaSyAgbZ8YHHPbaKWmEMzwI65jXflv-8qYCVM",
     authDomain: "schoolreport-f8db0.firebaseapp.com",
-    databaseURL: "https://schoolreport-f8db0-default-rtdb.firebaseio.com",
+    databaseURL: "https://schoolreport-f8db0-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "schoolreport-f8db0",
     storageBucket: "schoolreport-f8db0.firebasestorage.app",
     messagingSenderId: "313069994450",
     appId: "1:313069994450:web:37b009be4f1812fdca880b",
     measurementId: "G-N8YF0VKWCD"
 };
-
-// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-export { app, analytics };
+export { app, auth, db };
